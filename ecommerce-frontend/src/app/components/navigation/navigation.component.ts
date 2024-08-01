67d0ee892b72a1f355edf270a1e077af7f8550c9
ramihsasna <ramihsasna34@gmail.com>
2024-08-01 15:49:25 +0100

    Modified navigation components and added buttons

--- ecommerce-frontend/src/app/components/navigation/navigation.component.ts
+++ ecommerce-frontend/src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
+  imports: [
+    NzMenuModule,
+    NzLayoutModule,
+    RouterModule,
+    NzButtonModule,
+    NzIconModule,
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
