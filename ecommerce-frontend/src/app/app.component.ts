e839d9a1f2d64b983257846308df601b65f7bcc8
ramihsasna <ramihsasna34@gmail.com>
2024-08-01 15:50:20 +0100

    Added two carousel components that loop through an array of imageURLs

--- ecommerce-frontend/src/app/app.component.ts
+++ ecommerce-frontend/src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [
+    RouterModule,
+    NavigationComponent,
+    CarouselComponent,
+    FooterComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ecommerce-frontend';
+  carouselImages1 = [
+    { id: 1, imageUrl: '/macbook.png' },
+    { id: 2, imageUrl: '/picture2.jpg' },
+    { id: 3, imageUrl: '/picture3.jpg' },
+    { id: 4, imageUrl: '/picture4.jpg' },
+  ];
+
+  carouselImages2 = [
+    { id: 1, imageUrl: '/image1.jpg' },
+    { id: 2, imageUrl: '/image2.jpg' },
+    { id: 3, imageUrl: '/image3.jpg' },
+    { id: 4, imageUrl: '/image4.jpg' },
+  ];
 }
